7242e25cda88a0526a117f9dc57ff66937f0db0b
u-Kuro <rcdupitas@gmail.com>
2023-04-15 12:22:06 +0800

    fixed post count and query in db and connected it in front/back end

--- technews/config.php
+++ technews/config.php
@@ -3,7 +3,7 @@
   $conn=mysqli_connect("localhost","root","","technews") or die("connection failed ". mysqli_connect_error());
   // Interval Update for NEWSAPI
   $api_name = 'newsapi';
-  $sql = "SELECT last_update FROM api_interval WHERE api_name = '$api_name'";
+  $sql = "SELECT last_update FROM api_interval WHERE api_name = '{$api_name}'";
   $result = mysqli_query($conn, $sql);
   if (mysqli_num_rows($result) > 0) {
       $row = mysqli_fetch_assoc($result);
@@ -11,10 +11,10 @@
       // Calculate the time difference between the last update time and the current time
       $time_diff = time() - strtotime($last_update);
       $requestLimit = 100/5; // Current newsapi Limit is 100 divided by 5 different request
-      $intervalTime = 86400000 / $requestLimit; // interval request for 24 hrs (86400000 ms)
+      $intervalTime = 86400 / $requestLimit; // interval request for 24 hrs (86400 s)
       if ($time_diff >= $intervalTime) {
           // Update the last update time for the API in the database
-          $sql = "UPDATE api_interval SET last_update = NOW() WHERE api_name = '$api_name'";
+          $sql = "UPDATE api_interval SET last_update = NOW() WHERE api_name = '{$api_name}'";
           mysqli_query($conn, $sql);
           // Retrieve new data from the API
           require_once 'api/newsapi.php';
