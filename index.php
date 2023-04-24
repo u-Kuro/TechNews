9838b75c0bfbcdd7ad536a307b6f39f629089e83
u-Kuro <rcdupitas@gmail.com>
2023-04-24 14:21:32 +0800

    added manual news update/sms send and updated ui

--- index.php
+++
@@ -1,1 +0,0 @@
-<?php header("Location: technews/index.php"); ?>
--- technews/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); //variables value removed
-session_destroy();
-include "config.php";
-header("Location: login.php"); //go to index.php
-?>
